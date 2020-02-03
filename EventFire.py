8c4083ea8bdac58e68f65dfc53c4635bd9e8c8c2
Alexander Eder <30327526+Labi132@users.noreply.github.com>
2020-02-03 11:40:29 +0100

    Events, Tracking start
    Implemented events for tangibles, included prepared code for tracking. no functionality yet

---
+++ EventFire.py
@@ -0,0 +1,13 @@
+import pygame
+
+class EventFire:
+    def __init__(self):
+        self.alive = True
+
+    def tangible_move(self, tang):
+        my_event = pygame.event.Event(pygame.USEREVENT+1, who=tang)
+        pygame.event.post(my_event)
+
+    def tangible_death(self, tang):
+        my_event = pygame.event.Event(pygame.USEREVENT+2, who=tang)
+        pygame.event.post(my_event)
