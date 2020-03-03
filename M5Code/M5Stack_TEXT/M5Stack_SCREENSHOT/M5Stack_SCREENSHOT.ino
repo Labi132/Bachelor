e429f97ee3798a2833e08012d68056e7c4cab9f1
Labi132 <30327526+Labi132@users.noreply.github.com>
2020-03-03 14:58:47 +0100

    removed unnecessary init.pys, added multiTangibleStamp

---
+++ M5Code/M5Stack_TEXT/M5Stack_SCREENSHOT/M5Stack_SCREENSHOT.ino
@@ -0,0 +1,14 @@
+#include <M5Stack.h>
+
+void setup()
+{
+    delay(5000);
+    M5.begin(true, true, true);
+    M5.Lcd.setBrightness(255);
+    M5.Lcd.setTextWrap(true, true);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.setTextDatum(MC_DATUM);
+    M5.Lcd.drawString("Screenshot", 160, 120, 2);
+}
+
+void loop(){}
