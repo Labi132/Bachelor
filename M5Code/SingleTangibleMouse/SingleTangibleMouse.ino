cca4e0238ae16543e731003c8a12295b0128f6a2
Labi132 <30327526+Labi132@users.noreply.github.com>
2020-03-09 11:43:47 +0100

    fixed mouse lsit order removed weird headers

--- M5Code/SingleTangibleMouse/SingleTangibleMouse.ino
+++ M5Code/SingleTangibleMouse/SingleTangibleMouse.ino
@@ -34,22 +34,11 @@ void setup()
     
     
     M5.Lcd.setBrightness(255);
-<<<<<<< HEAD:M5Code/SingleTangibleMouse/SingleTangibleMouse.ino
     M5.Lcd.drawJpgFile(SD, "/drag.jpg", 0, 0, 320, 240);
 }
 
-String mode[5] = {"HIGHLIGHT", "DRAG", "GROUP", "PAN", "ZOOM"};
+String mode[5] = {"PAN", "DRAG","HIGHLIGHT", "GROUP",  "ZOOM"};
 int current_mode = 1;
-=======
-    M5.Lcd.setTextWrap(true, true);
-    M5.Lcd.setTextSize(3);
-    M5.Lcd.setTextDatum(MC_DATUM);
-    M5.Lcd.drawString("Stadt/City", 160, 120, 2);
-}
-
-String mode[6] = {"CITY", "FOOD", "PET", "SCREENSHOT", "VACATION", "ENTER"};
-int current_mode = 0;
->>>>>>> SingleTangibleStamp:M5Code/SingleStamp/SingleStamp.ino
 bool mode_changed = false;
 
 void loop()
@@ -60,11 +49,8 @@ void loop()
         //Serial.println(M5.BtnA.read());
         if (M5.BtnA.wasReleased()){
             if(current_mode == 0){
-<<<<<<< HEAD:M5Code/SingleTangibleMouse/SingleTangibleMouse.ino
                 current_mode = 4;
-=======
-                current_mode = 5;
->>>>>>> SingleTangibleStamp:M5Code/SingleStamp/SingleStamp.ino
+
                 mode_changed = true;
             }
             else{
@@ -73,12 +59,8 @@ void loop()
             }
         }
         if (M5.BtnC.wasReleased()){
-<<<<<<< HEAD:M5Code/SingleTangibleMouse/SingleTangibleMouse.ino
+
             if(current_mode == 4){
-=======
-            if(current_mode == 5){
->>>>>>> SingleTangibleStamp:M5Code/SingleStamp/SingleStamp.ino
-                current_mode = 0;
                 mode_changed = true;
             }
             else{
@@ -86,10 +68,6 @@ void loop()
                 mode_changed = true;
             }
         }
-<<<<<<< HEAD:M5Code/SingleTangibleMouse/SingleTangibleMouse.ino
-        
-=======
->>>>>>> SingleTangibleStamp:M5Code/SingleStamp/SingleStamp.ino
 
 
     // Use WiFiClient class to create TCP connections
@@ -101,23 +79,12 @@ void loop()
     }
 
     if (mode_changed == true){
-
-<<<<<<< HEAD:M5Code/SingleTangibleMouse/SingleTangibleMouse.ino
             switch(current_mode){
           case 0: M5.Lcd.clear(); M5.Lcd.drawJpgFile(SD, "/highlight.jpg", 0, 0, 320, 240); break;
           case 1: M5.Lcd.clear(); M5.Lcd.drawJpgFile(SD, "/drag.jpg", 0, 0, 320, 240); break;
           case 2: M5.Lcd.clear(); M5.Lcd.drawJpgFile(SD, "/group.jpg", 0, 0, 320, 240); break;
           case 3: M5.Lcd.clear(); M5.Lcd.drawJpgFile(SD, "/pan.jpg", 0, 0, 320, 240); break;
           case 4: M5.Lcd.clear(); M5.Lcd.drawJpgFile(SD, "/zoom.jpg", 0, 0, 320, 240); break;
-=======
-      switch(current_mode){
-          case 0: M5.Lcd.clear(); M5.Lcd.drawString("Stadt/City", 160, 120, 2); break;
-          case 1: M5.Lcd.clear(); M5.Lcd.drawString("Essen/Food", 160, 120, 2); break;
-          case 2: M5.Lcd.clear(); M5.Lcd.drawString("Haustier/Pet", 160, 120, 2); break;
-          case 3: M5.Lcd.clear(); M5.Lcd.drawString("Screenshot", 160, 120, 2); break;
-          case 4: M5.Lcd.clear(); M5.Lcd.drawString("Urlaub/Vacation", 160, 120, 2); break;
-          case 5: M5.Lcd.clear(); M5.Lcd.drawString("Ordner oeffnen", 160, 120, 2); break;
->>>>>>> SingleTangibleStamp:M5Code/SingleStamp/SingleStamp.ino
           default: break;
         }  
           // We now create a URI for the request
@@ -129,17 +96,6 @@ void loop()
                  "Host: " + host + "\r\n" +
                  "Connection: close\r\n\r\n");
     mode_changed = false;
-<<<<<<< HEAD:M5Code/SingleTangibleMouse/SingleTangibleMouse.ino
-=======
     client.stop();             
-    /* unsigned long timeout = millis();
-    while (client.available() == 0) {
-        if (millis() - timeout > 5000) {
-            Serial.println(">>> Client Timeout !");
-            client.stop();
-            return;
-         }
-       }*/
->>>>>>> SingleTangibleStamp:M5Code/SingleStamp/SingleStamp.ino
     }    
 }
